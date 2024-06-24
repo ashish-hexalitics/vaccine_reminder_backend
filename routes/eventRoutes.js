49560a58bf0352b099e8770112fa8a71da31f0c8
vasuhexa <vasu.rupolia@hexalitics.com>
2024-06-24 10:59:41 +0530

    Changed the name of the controller function from viewUpcominEvents to getUpcomingEvents

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const eventRouter = express.Router();
 
 eventRouter.post('/create_event', eventController.createEvent);
-eventRouter.get('/view_upcoming_events', eventController.viewUpcomingEvents);
+eventRouter.get('/view_upcoming_events', eventController.getUpcomingEvents);
 eventRouter.put('/edit_event', eventController.editEvent);
 eventRouter.put('/delete_event', eventController.deleteEvent);
 eventRouter.get('view_event', eventController.viewEvent);
